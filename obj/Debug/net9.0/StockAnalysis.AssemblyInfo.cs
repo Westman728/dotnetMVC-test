a28398b4c7d451c68f1f92f0748c5ee8e87f69e2
Westman728 <148480105+Westman728@users.noreply.github.com>
2025-04-01 17:07:19 +0200

    Update generated asset cache files and assembly info for consistency

--- obj/Debug/net9.0/StockAnalysis.AssemblyInfo.cs
+++ obj/Debug/net9.0/StockAnalysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockAnalysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160004def0936710ff749d1d0e06489a9de396f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94483164c9cb63b0416f376d797c1ca9916ec8b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockAnalysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockAnalysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
